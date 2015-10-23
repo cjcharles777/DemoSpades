7f83af9ea6a4d4e5bd1ea11c5c2e44f6980cf80c
Cedric Charles <ccharles@dittoholdings.com>
2015-10-23 16:41:21 -0500

    --Adding some socket.io love

--- src/js/main.js
+++ src/js/main.js
@@ -1,139 +1,436 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var playerCards, eastOppCards, northOppCards, westOppCards,
-    cardsInPlay;
-
-function preload()
+function Spades()
 {
-    game.load.image('spades_a', 'img/cards/Spades/A.png');
-    game.load.image('back_pomegranate', 'img/cards/Backs/Pomegranate.png');
-}
+    var self = this;
 
-function create() {
-    game.stage.backgroundColor = '#355e3b';
-    playerCards = game.add.group();
-    eastOppCards = game.add.group();
-    northOppCards = game.add.group();
-    westOppCards = game.add.group();
-    cardsInPlay = game.add.group();
-    var item;
+    var socket = io.connect('http://localhost:3700');
 
-    for (var i = 0; i < 13; i++)
-    {
-        item = playerCards.create(i*50, 0, 'spades_a');
-        item.inputEnabled = true;
-        item.events.onInputOver.add(lift);
-        item.events.onInputOut.add(drop);
-        item.events.onInputDown.add(confirmDoubleClick);
+    socket.on('disconnect', function (data) {
+        var msg = 'Connection loss :\\';
+        console.error(msg + "\n" + data);
+        window.alert(msg + "\n" + data);
+        location.href = location.href.replace(location.hash, "");
+    });
+    socket.on('error', function (data) {
+        console.error(data);
+        window.alert(data);
+        location.href = location.href.replace(location.hash, "");
+    });
+    socket.on('errorMsg', function (data) {
+        console.error("[Error:" + data.num + "] " + data.msg);
+        window.alert("Error " + data.num + "\n" + data.msg);
+        location.href = location.href.replace(location.hash, "");
+    });
+
+
+    var pingTime;
+    var ping;
+    socket.on('pong', function () {
+        latency = Date.now() - pingTime;
+        ping = latency;
+    });
+
+    function measurePing() {
+        setTimeout(function () {
+            pingTime = Date.now();
+            socket.emit('ping');
+            measurePing();
+        }, 2000);
     }
 
-    for (var i = 0; i < 13; i++)
-    {
-        item = eastOppCards.create(i*50, 0, 'back_pomegranate');
+    measurePing();
+    var gameDiv = "game";
+    var gameWidth = parseInt(document.getElementById(gameDiv).offsetWidth);
+    var gameHeight = parseInt(document.getElementById(gameDiv).offsetHeight);
 
-    }
-    for (var i = 0; i < 13; i++)
-    {
-        item = northOppCards.create(i*50, 0, 'back_pomegranate');
+    var debug = true;
 
-    }
-    for (var i = 0; i < 13; i++)
-    {
-        item = westOppCards.create(i*50, 0, 'back_pomegranate');
-
-    }
-
-    playerCards.scale.set(0.25,0.25);
-    playerCards.x = game.world.width/2 - (((30 * playerCards.length) + 130) *.25);
-    playerCards.y = game.world.height-125;
-
-    eastOppCards.scale.set(0.25,0.25);
-    eastOppCards.x = game.world.width - 125;
-    eastOppCards.y = game.world.height/2 + (((30 * eastOppCards.length) + 130) *.25);
-    eastOppCards.angle = -90;
-
-    northOppCards.scale.set(0.25,0.25);
-    northOppCards.x = game.world.width /2 + (((30 * northOppCards.length) + 130) * .25);
-    northOppCards.y = 125;
-    northOppCards.angle = 180;
-
-    westOppCards.scale.set(0.25,0.25);
-    westOppCards.x = 125;
-    westOppCards.y = game.world.height/2 - (((30 * westOppCards.length) + 130) *.25);
-    westOppCards.angle = 90;
+    var game = new Phaser.Game(gameWidth, gameHeight, debug ? Phaser.CANVAS : Phaser.AUTO, gameDiv, null, null, false, false);
+    var playerCards, eastOppCards, northOppCards, westOppCards,
+        cardsInPlay;
+    var currentPlayer = -1;
+    var master = false;
+    var paddles = [];
 
 
-    cardsInPlay.scale.set(0.25,0.25);
-    cardsInPlay.x = 400 - ((190 + 90)*.25);
-    cardsInPlay.y = 300 - (127.5*.25);
-}
+    var colors = ["ff0000", "00ff00", "0000ff", "ffff00"];
 
-function update()
-{
+    var BootState = {
+        preload: function () {
+            game.load.image('loadingBar', 'img/textures/loading.png');
+        },
+        create: function () {
+            game.state.start('preload');
+        }
+    };
 
-}
+    var LoadingState = {
+        preload: function () {
+            game.stage.disableVisibilityChange = true;
 
-function lift(item)
-{
-        if (item.y == 0 )
-        {
-            game.add.tween(item).to( { y: -380 }, 250, Phaser.Easing.Linear.None, true);
+            this.loadingBar = game.add.sprite(0, 0, 'loadingBar');
+            // Center the preload bar
+            this.loadingBar.x = game.world.centerX - this.loadingBar.width / 2;
+            this.loadingBar.y = game.world.centerY - this.loadingBar.height / 2;
+            game.load.setPreloadSprite(this.loadingBar);
+
+            game.load.image('spades_a', 'img/cards/Spades/A.png');
+            game.load.image('back_pomegranate', 'img/cards/Backs/Pomegranate.png');
+        },
+        create: function () {
+            this.loadingBar.destroy();
+
+            game.stage.backgroundColor = '#355e3b';
+            playerCards = game.add.group();
+            eastOppCards = game.add.group();
+            northOppCards = game.add.group();
+            westOppCards = game.add.group();
+            cardsInPlay = game.add.group();
+            var item;
+
+            for (var i = 0; i < 13; i++) {
+                item = playerCards.create(i * 50, 0, 'spades_a');
+                item.inputEnabled = true;
+                item.events.onInputOver.add(lift);
+                item.events.onInputOut.add(drop);
+                item.events.onInputDown.add(confirmDoubleClick);
+            }
+
+            for (var i = 0; i < 13; i++) {
+                item = eastOppCards.create(i * 50, 0, 'back_pomegranate');
+
+            }
+            for (var i = 0; i < 13; i++) {
+                item = northOppCards.create(i * 50, 0, 'back_pomegranate');
+
+            }
+            for (var i = 0; i < 13; i++) {
+                item = westOppCards.create(i * 50, 0, 'back_pomegranate');
+
+            }
+
+            playerCards.scale.set(0.25, 0.25);
+            playerCards.x = game.world.width / 2 - (((30 * playerCards.length) + 130) * .25);
+            playerCards.y = game.world.height - 125;
+
+            eastOppCards.scale.set(0.25, 0.25);
+            eastOppCards.x = game.world.width - 125;
+            eastOppCards.y = game.world.height / 2 + (((30 * eastOppCards.length) + 130) * .25);
+            eastOppCards.angle = -90;
+
+            northOppCards.scale.set(0.25, 0.25);
+            northOppCards.x = game.world.width / 2 + (((30 * northOppCards.length) + 130) * .25);
+            northOppCards.y = 125;
+            northOppCards.angle = 180;
+
+            westOppCards.scale.set(0.25, 0.25);
+            westOppCards.x = 125;
+            westOppCards.y = game.world.height / 2 - (((30 * westOppCards.length) + 130) * .25);
+            westOppCards.angle = 90;
+
+
+            cardsInPlay.scale.set(0.25, 0.25);
+            cardsInPlay.x = 400 - ((190 + 90) * .25);
+            cardsInPlay.y = 300 - (127.5 * .25);
+        },
+        update: function () {
+
+        }
+    };
+
+    var SynchState = {
+        p: false,
+        players: 0,
+        countdown: false,
+        init: function (data) {
+            game.stage.disableVisibilityChange = true;
+
+            var self = this;
+
+            self.players = parseInt(data.playersCount);
+            if (data.hosting) {
+                self.p = 0;
+                host = true;
+                socket.emit('startCounting', socket.id);
+            }
+            else {
+                self.p = data.playersCount - 1;
+                socket.on('joined', function (data) {
+                    self.players = parseInt(data.playersCount);
+                });
+                socket.on('playerLeft', function (data) {
+                    self.players = parseInt(data.playersCount);
+                });
+            }
+            socket.on('timeOut', function (data, ack) {
+                self.countdown = parseInt(data.times);
+                ack(socket.id);
+            });
+        },
+        preload: function () {
+            //TODO: Put things here
+        },
+        create: function () {
+            var style = {font: "30px Arial", fill: "#ffffff", align: "center"};
+            this.text = game.add.text(game.world.centerX, game.world.centerY, "Awaiting other players (" + this.players + "/4)", style);
+            this.text.anchor.setTo(0.5, 0.5);
+        },
+        update: function () {
+
+
+            if (this.countdown === false) {
+                //TODO: Add a demoMovements();
+            }
+            else {
+                this.initGame(this.countdown);
+            }
+
+            if (this.countdown !== false) {
+                this.text.text = "Ready to start...";
+            }
+            else if (host || this.players == 4) {
+                this.text.text = "Waiting for sync...";
+            }
+            else {
+                this.text.text = "Awaiting other players (" + this.players + "/4)";
+            }
+        },
+        initGame: function (phase) {
+            switch (phase) {
+                case 1:
+                //TODO: What does this do?
+                case 2:
+                    //TODO: What does this do?
+                    this.text.text = "GO!";
+                case 3:
+                    socket.removeAllListeners('joined');
+                    socket.removeAllListeners('timeOut');
+                    socket.removeAllListeners('playerLeft');
+                    this.text.destroy();
+                    game.state.start("game", false, false, {player: this.p});
+                    break;
+            }
+        }
+    };
+
+    var GameState = {
+        inactivePlayers: {0: false, 1: false, 2: false, 3: false},
+        gameRunning: false,
+        init: function (data) {
+            game.stage.disableVisibilityChange = true;
+
+            var self = this;
+            currentPlayer = data.player;
+            master = data.player == 0;
+
+            socket.on('playerLeft', function (data) {
+                self.inactivePlayers[parseInt(data.playerLeft)] = true;
+            });
+            socket.on('clientUpdate', function (data) {
+                self.updateClient(data);
+            });
+            socket.on('clientUpdateScores', function (data) {
+                self.updateClientScores(data);
+            });
+            // socket.on('clientUpdateBall', function (data) {
+            //      self.updateClientBall(data);
+            //  });
+            socket.on('becomeHost', function (data) {
+                master = true;
+                //TODO: What does the host know?
+            });
+        },
+        create: function () {
+            if (!master) {
+                //ball.body.velocity.x = 0;
+                //ball.body.velocity.y = 0;
+            }
+            else {
+
+            }
+
+            var scoresPos = [
+                {w: game.world.centerX, h: game.world.centerY - 100},
+                {w: game.world.centerX - 100, h: game.world.centerY},
+                {w: game.world.centerX, h: game.world.centerY + 100},
+                {w: game.world.centerX + 100, h: game.world.centerY}
+            ];
+
+            for (var i in paddles) {
+                // paddles[i].position.setTo(paddles[i].op.x,paddles[i].op.y);
+                var style = {font: "50px Arial", fill: "#" + colors[i], align: "center"};
+                paddles[i].scoreLabel = game.add.text(scoresPos[i].w, scoresPos[i].h, "0", style);
+                paddles[i].scoreLabel.anchor.setTo(0.5, 0.5);
+            }
+
+            this.gameRunning = true;
+        },
+        update: function () {
+            if (this.gameRunning) {
+
+                this.updateServer();
+            }
+        },
+        checkScore: function () {
+            //TODO:Scoring Here
+        },
+        inputManagement: function () {
+            //TODO: How do we handle the inputs here.
+        },
+        endGame: function (player) {
+            this.gameRunning = false;
+
+            //TODO:Destroy all
+
+
+            var style = {font: "50px Arial", fill: "#ffffff", align: "center"};
+            var wonSentence = player.player == currentPlayer ? "You win!" : player.name + " wins!";
+            var text = game.add.text(game.world.centerX, game.world.centerY, wonSentence, style);
+            text.anchor.setTo(0.5, 0.5);
+
+            socket.removeAllListeners('playerLeft');
+            socket.removeAllListeners('clientUpdate');
+            socket.removeAllListeners('clientUpdateScores');
+            //socket.removeAllListeners('clientUpdateBall');
+            socket.removeAllListeners('becomeHost');
+            socket.removeAllListeners('disconnect');
+            socket.removeAllListeners('errorMsg');
+            socket.removeAllListeners('error');
+
+            $("#connect tr").addClass("hide");
+            $("#endContainer").removeClass("hide");
+            $("#connect").removeClass("hide").css("background-color", "transparent");
+
+            setTimeout(function () {
+                socket.disconnect();
+            }, 5000);
+        },
+        socketTiming: 0,
+        socketDelay: 16,
+        getSocketDelay: function () {
+            return ping < this.socketDelay ? ping : this.socketDelay;
+        },
+        updateServer: function () {
+            this.socketTiming += game.time.elapsed;
+            if (this.socketTiming < this.getSocketDelay()) {
+                return;
+            }
+            this.socketTiming = 0;
+            var data = {socketId: socket.id};
+
+            if (master) {
+                //TODO: update data for world
+            }
+            else {
+                //TODO: Update data for self
+            }
+
+            data['player'] = parseInt(currentPlayer);
+            switch (data['player']) {
+                case 0:
+                case 2:
+                case 1:
+                case 3:
+                    //TODO: update player data
+                    break;
+            }
+
+            socket.emit('gameUpdate', data);
+        },
+        updateClient: function (data) {
+            //TODO: Update Clients
+        },
+        updateClientScores: function (data) {
+            if (!master) {
+
+            }
+        },
+        updateClientBall: function (data) {
+            if (!master) {
+
+            }
+        },
+        render: function () {
+            //       if (debug) {
+            //          game.debug.body(ball);
+            //          for (var i in paddles) {
+            //               game.debug.body(paddles[i]);
+            //           }
+            //       }
+        }
+    };
+
+    game.state.add("boot", BootState, true);
+    game.state.add("preload", LoadingState, false);
+    game.state.add("sync", SynchState, false);
+    game.state.add("game", GameState, false);
+
+    this.switchToSync = function (data) {
+        game.state.start("sync", false, false, data);
+    };
+
+    this.getSocket = function () {
+        return socket;
+    };
+
+
+    function lift(item) {
+        if (item.y == 0) {
+            game.add.tween(item).to({y: -380}, 250, Phaser.Easing.Linear.None, true);
         }
 
 
-
-
-
-
-}
-function drop(item)
-{
-    var tween = getTween(item);
-    if (item.y === -380 )
-    {
-        tween.to({y: 0}, 250, Phaser.Easing.Linear.None, true);
-    }
-    else if(item.y > -380 && item.y <= 0)
-    {
-        tween.onComplete.addOnce(function(){tween.to({y: 0}, 250, Phaser.Easing.Linear.None, true);});
     }
 
-
-
-}
-
-function getTween(item)
-{
-    var gameTweens = game.tweens.getAll();
-    for(var x in gameTweens)
-    {
-        if(gameTweens[x].target == item)
-        {
-            return gameTweens[x];
+    function drop(item) {
+        var tween = getTween(item);
+        if (item.y === -380) {
+            tween.to({y: 0}, 250, Phaser.Easing.Linear.None, true);
         }
+        else if (item.y > -380 && item.y <= 0) {
+            tween.onComplete.addOnce(function () {
+                tween.to({y: 0}, 250, Phaser.Easing.Linear.None, true);
+            });
+        }
+
+
     }
 
-    return game.add.tween(item);
-}
+    function getTween(item) {
+        var gameTweens = game.tweens.getAll();
+        for (var x in gameTweens) {
+            if (gameTweens[x].target == item) {
+                return gameTweens[x];
+            }
+        }
 
-
-function confirmDoubleClick (itemBeingClicked)
-{
-    if (!this.secondClick)
-    {
-        this.secondClick = true;
-        this.time.events.add(300, function(){
-            this.secondClick = false;
-        },this);
-        return;
+        return game.add.tween(item);
     }
 
-    putCardInPlay(itemBeingClicked)
+
+    function confirmDoubleClick(itemBeingClicked) {
+        if (!this.secondClick) {
+            this.secondClick = true;
+            this.time.events.add(300, function () {
+                this.secondClick = false;
+            }, this);
+            return;
+        }
+
+        putCardInPlay(itemBeingClicked)
+    }
+
+    function putCardInPlay(item) {
+        cardsInPlay.add(item);
+        item.inputEnabled = false;
+    }
+
+
+    return this;
 }
 
-function putCardInPlay(item)
-{
-    cardsInPlay.add(item);
-    item.inputEnabled = false;
-}
-
-
+Spades.prototype.getSocket = function () {
+    return this.getSocket();
+};
+Spades.prototype.sync = function(data) {
+    this.switchToSync(data);
+};
--- src/js/multiplayer.js
+++ src/js/multiplayer.js
@@ -1,42 +1,77 @@
 /**
  * Created by cedric on 10/19/15.
  */
+$(function() {
+    var hash = false;
+    if (window.location.hash.length > 0) {
+        hash = window.location.hash.substr(1);
+    }
 
-var self = this;
+    var spades = new Spades();
+    var socket = spades.getSocket();
 
-var socket = io.connect('http://localhost:3700');
+    $("#buttonsContainer").removeClass("hide");
 
-socket.on('disconnect', function(data) {
-    var msg = 'Connection loss :\\';
-    console.error(msg+"\n"+data);
-    window.alert(msg+"\n"+data);
-    location.href = location.href.replace(location.hash,"");
+    $("#hostGame").click(function () {
+        $("#buttonsContainer").addClass("hide");
+        $("#qrcodeContainer").removeClass("hide");
+
+        var $playersCount= $("#playersCount");
+
+        socket.on('joined', function(data) {
+            data.playersCount = parseInt(data.playersCount);
+
+            if ($playersCount.length > 0) {
+                $playersCount.text(data.playersCount);
+            }
+            if (data.playersCount == 4) {
+                $("#connect").addClass("hide");
+                spades.sync({ hosting: true, playersCount: data.playersCount });
+            }
+        });
+        socket.on('playerLeft', function (data) {
+            data.playersCount = parseInt(data.playersCount);
+
+            if ($playersCount.length > 0) {
+                $playersCount.text(data.playersCount);
+            }
+            if (data.playersCount == 4) {
+                $("#connect").addClass("hide");
+                spades.sync({ hosting: true, playersCount: data.playersCount });
+            }
+        });
+        socket.emit('host', '', function(data) {
+            $('#qrcode').qrcode({width: 245,height: 245,text: window.location.href+"#"+data });
+
+            $("#gameId").text(data);
+        });
+    });
+
+    $("#joinGame").click(function () {
+        $("#buttonsContainer").addClass("hide");
+        $("#formContainer").removeClass("hide");
+
+        if (hash.length > 0) {
+            $("#inputGameId").val(hash);
+        }
+        $("#inputGameId").select();
+    });
+
+    $("#joinGameId").click(function () {
+        $("#connect").addClass("hide");
+
+        socket.emit('join', $("#inputGameId").val().trim(), function(data) {
+            spades.sync({ hosting: false, playersCount: parseInt(data.playersCount) });
+        });
+    });
+
+    $("#restartGame").click(function () {
+        socket.removeAllListeners('disconnect');
+        socket.disconnect();
+        location.href = location.href.replace(location.hash,"");
+    });
+
+    if (hash.length > 0) {
+        $("#joinGame").trigger('click');
+    }
 });
-socket.on('error', function(data) {
-    console.error(data);
-    window.alert(data);
-    location.href = location.href.replace(location.hash,"");
-});
-socket.on('errorMsg', function(data) {
-    console.error("[Error:"+data.num+"] "+data.msg);
-    window.alert("Error "+data.num+"\n"+data.msg);
-    location.href = location.href.replace(location.hash,"");
-});
-
-
-var pingTime;
-var ping;
-socket.on('pong', function () {
-    latency = Date.now() - pingTime;
-    ping = latency;
-});
-
-function measurePing() {
-    setTimeout(function () {
-        pingTime = Date.now();
-        socket.emit('ping');
-        measurePing();
-    }, 2000);
-}
-measurePing();
-
